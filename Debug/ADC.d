9017b8815a97028f7a930b33846f2e1a830e2daf
badr-jr <74314248+badr-jr@users.noreply.github.com>
2021-03-16 10:32:07 +0200

    Delete Debug directory

--- Debug/ADC.d
+++
@@ -1,9 +0,0 @@
-ADC.d ADC.o: .././ADC.c ../ADC.h ../Types.h ../Registers.h ../Macros.h
-
-../ADC.h:
-
-../Types.h:
-
-../Registers.h:
-
-../Macros.h:
--- Debug/DIO.d
+++
@@ -1,7 +0,0 @@
-DIO.d DIO.o: .././DIO.c .././Types.h .././Registers.h .././Macros.h
-
-.././Types.h:
-
-.././Registers.h:
-
-.././Macros.h:
